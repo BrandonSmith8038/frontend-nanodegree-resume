28f1c82a424e942f1408e8b927af5a8e681a6cbe
Brandon Smith <brandon@reddirtwebdesign.com>
2017-07-25 14:03:32 -0700

    Finished Lesson 9

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,74 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append(["Brandon Smith"]);
+
+// var awesomeThoughts = "I am Brandon and I am AWESOME!";
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $('#main').append(funThoughts);
+var bio = {
+	"name": "Brandon",
+	"role": "Front End Developer",
+	"welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, sit.",
+	"bioPic": "images/profilePicture.jpg",
+	"contacts": {
+		"mobile": "(602)-694-0239",
+		"email": "brandon@reddirtwebdesign.com",
+		"github": "https://github.com/BrandonSmith8038",
+		"location": "Goodyear, AZ" 
+	},
+	"skills": [
+	"HTML","CSS","Javascript","Powershell"
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Sample Project 1",
+			"dates": "2013-2014",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus laboriosam debitis culpa expedita est accusamus, quas dolores sit fuga, reprehenderit."
+		},
+		{
+			"title": "Sample Project 2",
+			"dates": "2015-2017",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus natus aperiam dolores quidem perspiciatis aspernatur quia sed nam, neque est!"
+		}
+	]
+}
+
+var work = {
+	"jobs": [
+		{
+		"employer": "Scottrade Inc",
+		"title": "Infastructure Operations Technician",
+		"location": "Scottsdale, AZ",
+		"dates": "April 2016-Present",
+		"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit blanditiis ipsam similique reprehenderit eveniet iste quaerat aperiam est praesentium vel."
+		},
+		{
+		"employer": "Cloud Blue Technologies",
+		"title": "Computer Technician",
+		"location": "Scottsdale, AZ",
+		"dates": "May 2014-March 2016",
+		"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ad, laborum reiciendis eum delectus, voluptatem quaerat sit nemo illum pariatur."
+		}
+	]
+}
+
+var education = {
+	"schools": [
+		{
+		"name": "Estrella Mountain Community College",
+		"location": "Avondale, AZ",
+		"degree": "Assocaite in Computer Science",
+		"dates": "2011-2014",
+		"url": "#"
+	},
+		{
+		"name": "Glendale Community College",
+		"location": "Glendale, AZ",
+		"degree": " ",
+		"dates": "2005",
+		"url": "#"	
+		}
+	]
+}
